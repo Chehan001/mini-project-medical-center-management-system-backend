c09f08cc53a42d2a09e682fb807441864d4c078b
Chehan Lasindu <clmallikage001@gmail.com>
2025-12-10 14:37:40 +0530

    Server-&-Environmental-Update-As-Before

--- server.js
+++ server.js
@@ -31,26 +31,8 @@ import universityMedicalRoutes from "./routes/universityMedicalRoutes.js";
 import otherMedicalRoutes from "./routes/otherMedicalRoutes.js";
 import studentMedicalRoutes from "./routes/studentMedicalRoutes.js";
 
-// CORS Middleware 
-const allowedOrigins = [
-  "http://localhost:3000", 
-  "https://mini-project-medical-center-managem-pink.vercel.app" 
-];
-
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true); // allow server-to-server or Postman requests
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `CORS policy does not allow access from the specified origin.`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true,
-}));
-
 // Middleware
-//app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
